e1fb8a6cc24d39e1e9ed6b0373ff29e0f0ab610f
Geison Amorim <geisonam43@gmail.com>
2022-04-11 21:35:11 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+            double A, R, pi = 3.14159;
+
+            //Entrada
+
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Processamento
+
+            A = pi * R * R;
+
+            //Saida
+
+            Console.WriteLine("Area =" + A.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
